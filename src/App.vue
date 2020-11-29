c2d72d7880f824e7cec7ae59a6014e22a9e01ba6
CXT <cxt77965@163.com>
2020-11-29 20:10:18 +0800

    1.完成主页样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <template>
-  <div></div>
+  <div id="player">
+      <h2 class="title">黑云音乐</h2>
+      <div class="search">
+        <input type="text"/>
+        <button>
+          <span class="iconfont icon-search"></span>
+        </button>
+      </div>
+      <div class="tab-wrapper">
+        <!-- tab栏 -->
+        <div class="tab-bar">
+          <a class="bar-item active">
+            搜索结果
+          </a>
+          <a class="bar-item">
+            歌曲播放
+          </a>
+          <a class="bar-item">
+            mv
+          </a>
+          <a class="bar-item">
+            歌曲评论>
+          </a>
+         
+        </div>
+        <!-- 对应的内容区域 -->
+        <div class="tab-content">
+
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +39,6 @@ export default {
 </script>
 
 <style>
-
+  @import url('./assets/css/index.css');
+  @import url('./assets/css/iconfont.css');
 </style>
