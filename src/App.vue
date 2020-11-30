e601b0fbb172b4f6391b6f1bc00426cb8404203f
CXT <cxt77965@163.com>
2020-11-30 16:24:26 +0800

    8.完成黑云音乐

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="player">
-    <h2 class="title">黑云音乐</h2>
+    <h2 class="title" @click="blackCloud">黑云音乐</h2>
     <div class="search">
       <input type="text" v-model="keywords" @keyup.enter="searchMusic" />
       <button @click="searchMusic">
@@ -17,17 +17,23 @@ export default {
   name:'index',
   data() {
     return {
-      keywords:'周杰伦' //搜索关键字
+      keywords:'' //搜索关键字
     }
   },
   methods:{
     //点击搜索事件
-    searchMusic(){
-      // console.log(11111);
+    searchMusic() {
       //去空格
       this.keywords = this.keywords.trim()
       //this.$router这个是路由实例
       this.$router.push(`/results/${this.keywords}`)
+    },
+
+    //点击标题回到首页
+    blackCloud() {
+       this.$router.push('/')
+
+       this.keywords = ''
     }
   }
 };
